36afdc456a95d229e1ab361655381dae23efbd5a
jkarnam <karnam.jaswanth@outlook.com>
2021-06-28 20:21:49 +0530

    added few lines to code

--- app.py
+++ app.py
@@ -1,2 +1,14 @@
 #this is develpment branch 
 #lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git
+#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git
