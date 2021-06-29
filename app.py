8c457d72b3aa0e5e74cc5ba89dd688177653a69b
jkarnam <karnam.jaswanth@outlook.com>
2021-06-29 12:33:09 +0530

    new few lines of code removed

--- app.py
+++ app.py
@@ -23,8 +23,5 @@
 #git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
 #lfgskfgjfskgnlfkgnflkgnflkgn
 #dfmdkpfndkfndpsikfndkfndslkfn
-#git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
-#lfgskfgjfskgnlfkgnflkgnflkgn
-#dfmdkpfndkfndpsikfndkfndslkfn
-#git remote add origin https://github.com/jkarnam7/gitlabsss.git
+
 
