6a56aa744fadfa1ddacbbe0453159919c94c76b7
jkarnam <karnam.jaswanth@outlook.com>
2021-06-29 12:31:49 +0530

    new few lines of code added

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#this is develpment branch 
+#this is master branch code 
 #lfgskfgjfskgnlfkgnflkgnflkgn
 #dfmdkpfndkfndpsikfndkfndslkfn
 #git remote add origin https://github.com/jkarnam7/gitlabsss.git
@@ -12,3 +12,19 @@
 #lfgskfgjfskgnlfkgnflkgnflkgn
 #dfmdkpfndkfndpsikfndkfndslkfn
 #git remote add origin https://github.com/jkarnam7/gitlabsss.git
+
+#this is master branch code 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git
+#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
+#lfgskfgjfskgnlfkgnflkgnflkgn
+#dfmdkpfndkfndpsikfndkfndslkfn
+#git remote add origin https://github.com/jkarnam7/gitlabsss.git
+
