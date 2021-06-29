43d89f03016d89a1ce315b6e18f24c22181469e1
jkarnam <karnam.jaswanth@outlook.com>
2021-06-29 12:34:05 +0530

    new lines of code removeded

--- app.py
+++ app.py
@@ -21,7 +21,6 @@
 #lfgskfgjfskgnlfkgnflkgnflkgn
 #dfmdkpfndkfndpsikfndkfndslkfn
 #git remote add origin https://github.com/jkarnam7/gitlabsss.git#this is develpment branch 
-#lfgskfgjfskgnlfkgnflkgnflkgn
-#dfmdkpfndkfndpsikfndkfndslkfn
+
 
 
